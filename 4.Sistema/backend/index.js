0ce3734be310839d57880c990124e94a62bbfb8d
PauloVieira <paulovieira@discente.ufg.br>
2022-02-03 21:14:09 -0300

    arquivo Asta

--- 4.Sistema/backend/index.js
+++ 4.Sistema/backend/index.js
@@ -3,8 +3,17 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('From Get')
 })
+
+app.get('/projetos', (req, res) => {
+    res.send('Path Projetos')
+})
+
+app.post('/', (req, res) => {
+    res.send('From Post!')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
