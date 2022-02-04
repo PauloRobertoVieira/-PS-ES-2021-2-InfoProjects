a7b06a711116728136a2419af6a43325484807d4
PauloVieira <paulovieira@discente.ufg.br>
2022-02-03 22:31:45 -0300

    Endpoint projetos

--- 4.Sistema/backend/index.js
+++ 4.Sistema/backend/index.js
@@ -1,19 +1,49 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('From Get')
 })
 
 app.get('/projetos', (req, res) => {
-    res.send('Path Projetos')
+  const projetos = [
+    {
+      id: 1,
+      name: 'Projeto1'
+    },
+    {
+      id: 2,
+      name: 'Projeto2'
+    }
+  ]
+    res.json(projetos)
 })
 
-app.post('/', (req, res) => {
-    res.send('From Post!')
+app.get('/projetos/:id', (req, res) => {
+  console.log(req.params)
+   res.send(`Return post ${req.params.id}`)
 })
 
+app.delete('/projetos/:id', (req, res) => {
+  console.log(req.params)
+   res.send(`Delete post ${req.params.id}`)
+})
+
+app.put('/projetos/:id', (req, res) => {
+  console.log(req.params)
+  const post = req.body
+   res.send(`UpDate post ${req.params.id}`)
+})
+
+app.post('/projetos', (req, res) => {
+  console.log(req.body)
+  const post = req.body
+  res.send(`Save post ${post.id}`)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
