c175aec9ad4dc0a1a1525426df0f996cfef14f89
PauloVieira <paulovieira@discente.ufg.br>
2022-02-16 16:56:40 -0300

    configruações iniciais projeto, paginas e rotas

---
+++ 4.Sistema/frontend/infoprojects/src/App.js
@@ -0,0 +1,43 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
+
+function App() {
+  return (
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo projeto</Link>
+      </ul>
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path='/'>
+            <Home />
+          </Route>
+
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+
+          <Route path='/company'>
+            <Company />
+
+          </Route>
+
+          <Route path='/newproject'>
+            <NewProject />
+          </Route>
+        </Container>
+      </Switch>
+      <p>Footer</p>
+    </Router>
+  );
+}
+
+export default App;
