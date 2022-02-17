ec261a047c465b6e3aa9d7e5e1022319165ee883
PauloVieira <paulovieira@discente.ufg.br>
2022-02-17 13:18:29 -0300

    página home

--- 4.Sistema/frontend/infoprojects/src/components/pages/Home.js
+++ 4.Sistema/frontend/infoprojects/src/components/pages/Home.js
@@ -1,8 +1,14 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+
 function Home(){
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>InfoProjects</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <a href='/'>Criar Projeto</a>
+            <img src={savings} alt='InfoProjects' />
+        </section>
     )
 }
 
