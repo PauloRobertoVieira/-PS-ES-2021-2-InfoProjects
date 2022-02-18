e4cc7d800021f1191826b2a263de2f49c2502e27
PauloVieira <paulovieira@discente.ufg.br>
2022-02-18 14:50:11 -0300

    component mensagem

--- 4.Sistema/frontend/infoprojects/src/components/pages/NewProject.js
+++ 4.Sistema/frontend/infoprojects/src/components/pages/NewProject.js
@@ -21,7 +21,7 @@ function NewProject (){
             .then((data) => {
                 console.log(data)
                 //redirect
-                history.push('/projects', { message: 'Projeto criad com sucesso!'})
+                history.push('/projects', { message: 'Projeto criado com sucesso!'})
             })
             .catch(err => console.log(err))
     }
