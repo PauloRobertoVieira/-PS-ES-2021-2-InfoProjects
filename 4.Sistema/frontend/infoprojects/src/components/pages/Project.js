4caf2c599abdeb27a369a2d89d16e0277df691c2
PauloVieira <paulovieira@discente.ufg.br>
2022-02-19 10:33:02 -0300

    metodo edicao projeto (PATCH)

--- 4.Sistema/frontend/infoprojects/src/components/pages/Project.js
+++ 4.Sistema/frontend/infoprojects/src/components/pages/Project.js
@@ -1,4 +1,8 @@
 import styles from './Project.module.css'
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
+import ProjectForm from '../project/ProjectForm'
+import Message from '../layout/Message'
 
 import {useParams} from 'react-router-dom'
 import {useState, useEffect} from 'react'
@@ -8,22 +12,95 @@ function Project (){
     const {id} = useParams()
 
     const [project, setProject] = useState([])
+    const [showProjectForm, setShowProjectForm] = useState(false)
+    const [message, setMessage] = useState()
+    const [type, setType] = useState()
 
     useEffect(() => {
-        fetch(`http://localhost:5000/projects/${id}`, {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-    .then(resp => resp.json())
-    .then((data) => {
-        setProject(data)
-    })
-    .catch(err => console.log)
+        setTimeout(()=>{
+            fetch(`http://localhost:5000/projects/${id}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+                setProject(data)
+            })
+            .catch((err) => console.log)
+        }, 2000)
     }, [id])
+
+    function editPost(project){
+        //budget validation
+        if(project.budget < project.infoproject){
+            setMessage('O orçamento não pode ser menor que o custo do projeto!')
+            setType('error')
+            return false
+        }
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+                setProject(data)
+                setShowProjectForm(false)
+                setMessage('Projeto atualizado')
+                setType('success')
+            })
+            .catch((err) => console.log)
+    }
+
+    function toggleProjectForm(){
+        setShowProjectForm(!showProjectForm)
+    }
     
-    return <p>{project.name}</p>
+    return(
+        <>
+        {project.name ? (
+        <div className={styles.project_details}>
+            <Container customClass="column">
+                {message && <Message type={type} msg={message}/>}
+            <div className={styles.details_container}>
+                <h1>Projeto: {project.name}</h1>
+                <button className={styles.btn} onClick={toggleProjectForm}>
+                {!showProjectForm ? 'Editar Projeto' : 'Fechar'}
+                </button>
+            {!showProjectForm ? (
+                <div className={styles.project_info}>
+                    <p>
+                        <span>Categoria: </span>{project.category.name}
+                    </p>
+                    <p>
+                        <span>Total de Orçamento:</span> R${project.budget}
+                    </p>
+                    <p>
+                        <span>Total Utilizado:</span> R${project.infoproject}
+                    </p>
+                </div>
+            ) : (
+                <div className={styles.project_info}>
+                    <ProjectForm
+                     handleSubmit={editPost}
+                     btnText="Concluir edição"
+                     projectData={project} 
+                    />
+                </div>
+            )}
+            </div>
+            </Container>
+        </div>
+        ) : (
+        <Loading />
+        )}
+        </>
+    )    
 }
 
 export default Project
